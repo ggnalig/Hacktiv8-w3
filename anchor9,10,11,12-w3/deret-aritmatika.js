96d2d31d343e8ae1fd3483180321a3910f5ec6e1
unknown <rahmatgilangmaulana9@gmail.com>
2020-04-12 11:53:47 +0700

    final

--- anchor9,10,11,12-w3/deret-aritmatika.js
+++ anchor9,10,11,12-w3/deret-aritmatika.js
@@ -1,18 +1,21 @@
 function tentukanDeretAritmatika(arr) {
-    b = 0;
-    a = 0;
-    for (let i = 0; i <= arr.length-2; i++) {
-        b = (arr[i+1] - arr[i]);  
+    let tempb = '';
+    let tempa = '';
+    for (let i = 0; i < arr.length-1; i++) {
+        b = (arr[i+1] - arr[i]);
+        tempb += b;
     } 
-    for (let j = arr.length-1; 0 < j; j--) {
+    for (let j = arr.length - 1; 0 < j; j--) {
         a = (arr[j] - arr[j-1]);
+        tempa += a;
     } 
-    if (a == b) {
+    if (tempa == tempb) {
         return true;
     } else {
         return false;
     }
 }
+
 
 // TEST CASES
 console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
@@ -21,6 +24,7 @@ console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
 console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
 console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
 console.log(tentukanDeretAritmatika([1, 3, 5, 7]));
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 1, 2]));
 
 
 /* Un = a + (n – 1) b */
